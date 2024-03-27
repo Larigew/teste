46803901bedd1b4f04a8ca69b0ed3b45bc953ede
Larigew <larissatgew@gmail.com>
2024-03-27 16:09:37 -0300

    commit top da larissa

--- js/script.js
+++ js/script.js
@@ -10,6 +10,9 @@ let notes = document.querySelector('#notes');//Lista divs com dados das notas
 let btnSaveNote = document.querySelector("#btn-save-note"); //icone para salvar nota
 let btnCloseNote = document.querySelector("#btn-close-note");//icone para fechar modal de edição de nota.
 
+let btnEditNote = document.querySelector("#btnEdit");
+let btnExcluirNote = document.querySelector("#btnDelete");
+
 addNote.addEventListener("click", (evt)=>{
     evt.preventDefault();
     notes.style.display = "none";
@@ -17,11 +20,14 @@ addNote.addEventListener("click", (evt)=>{
     addNote.style.display = "none";
 });
 
+
+
 btnCloseNote.addEventListener("click", () =>{
     evt.preventDefault();
     notes.style.display = "flex";
     modal.style.display = "none";
     addNote.style.display = "block";
+    document.querySelector('#input-id').value = "";
 })
 
 btnSaveNote.addEventListener("click", (evt) =>{
@@ -34,30 +40,35 @@ btnSaveNote.addEventListener("click", (evt) =>{
     saveNote (data);
 })
 
+//-----------------------FUNÇÕES----------------------
 
 
 const saveNote = (note) => {
 
-    let notes = leadNotes();
+    let notes = loadNotes();
 
     note.lastTime = new Date().getTime();
-    console.log(note.lastTime)
+
     if (note.id.length > 0){
-        
+        note.id=parseInt(note.id)
+        notes.forEach((item, i)=>{
+            if(item.id===note.id){
+                notes[i]=  note;
+            }
+        })
     }
     else{  
         note.id = new Date().getTime();
+        document.querySelector('#input-id').value=note.id;
+        notes.push(note);
     }
 
-    console.log(notes);
-    notes.push(note);
     notes = JSON.stringify(notes);
-    console.log(notes)
     localStorage.setItem('notes',notes);
     
 
 };
-const leadsNotes = ( ) =>{
+const loadNotes = ( ) =>{
     let notes = localStorage.getItem('notes');
     if (!notes){
         notes = [];
@@ -68,3 +79,63 @@ const leadsNotes = ( ) =>{
 
     return notes;
 }
+
+const listNotes = () => {
+    let listNotes = localStorage.getItem("notes");
+    listNotes = JSON.parse(listNotes);
+    notes.innerHTML = "";
+    console.log(listNotes)
+    listNotes.forEach((item) => {
+      const divCard = document.createElement("div");
+      divCard.className = "card";
+      divCard.style.width = "18rem";
+      const divCardBody = document.createElement("div");
+      divCardBody.className = "card-body";
+      const h1 = document.createElement("h1");
+      h1.innerText = item.title;
+      const pContent = document.createElement("p");
+      pContent.innerText = item.content;
+      const pLastTime = document.createElement("p");
+      let lastTime = new Date (item.lastTime).toLocaleDateString('pt-BR');
+      pLastTime.innerText = "ulima alteração:" +lastTime;
+      divCardBody.appendChild(h1);
+      divCardBody.appendChild(pContent);
+      divCardBody.appendChild(pLastTime);
+      divCard.appendChild(divCardBody);
+      notes.appendChild(divCard);
+      divCard.addEventListener("click", (evt)=>{
+            evt.preventDefault();
+            showNote(item)
+        })
+    });
+  };
+
+  const showNote = (note) => {
+    notes.style.display = 'none';
+    addNote.style.display = 'none';
+    modalView.style.display = 'block';
+    document.querySelector('#title-note').innerHTML = "<h1>"+note.title+"</h1>";
+
+    document.querySelector('#content-note')
+    .appendChild(document.createElement('p')
+    .appendChild(document.createTextNode(note.content)));
+
+    document.querySelector('#content-note')
+    .appendChild(document.createElement('p')
+    .appendChild(document.createTextNode(
+        new Date(note.lastTime).toLocaleDateString('pt-BR'))))
+
+        btnEditNote.addEventListener("click", (evt)=>{
+        evt.preventDefault();
+        notes.style.display = 'none';
+        modalView.style.display = 'none';
+        modal.style.display = 'block';
+        document.querySelector("#input-id").value = note.id;
+        document.querySelector("#input-title"). value = note.title;
+        document.querySelector("#input-content").value = note.content;
+        })
+
+  }
+  listNotes();
+
+listNotes();
